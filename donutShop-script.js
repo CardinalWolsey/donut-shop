8f32cd7ea9dc788b5f3868a78e9d922041c92c79
Spencer Caldwell <spencer.caldwell@gmail.com>
2015-07-13 13:53:34 -0700

    Set up all the files and laid out basic HTML structure of table

---
+++ donutShop-script.js
@@ -0,0 +1,13 @@
+// This script will imput values into a table to represent a model of the performance of a chain of donut shops
+
+
+var DonutEntry = function(number, time) {
+  this.number = number;
+  this.time = time;
+};
+
+DonutEntry.prototype.render = function() {
+  var addEntry = document.createElement(){
+
+  }
+}
